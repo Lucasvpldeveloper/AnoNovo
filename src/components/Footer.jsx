48376b96fcc18badcdfa3545bc22c0533b020ece
Lucasvpl <lucasvpldeveloper@gmail.com>
2025-01-16 20:27:42 -0300

    Footer finalizado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,17 @@
-
-
+import './Style.module.scss'
+import logoFacebook from "../assets/logoFacebook.png"
+import logoInstagram from "../assets/logoInstagram.png"
+import logoLinkedin from "../assets/logoLinkedin.png"
 
 
 export default function Footer(){
     return(
         <footer>
-            footer
+            <section>
+                <img src={logoLinkedin} alt="Logo do linkedin" />
+                <img src={logoFacebook} alt="Logo do facebook" />
+                <img src={logoInstagram} alt="Logo do instagram" />
+            </section>
         </footer>
     )
 }
